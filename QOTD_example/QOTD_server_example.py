df47aab98fe9d48e159125b696f98f653328f65b
stanislav <stanislav.matveew@yandex.ru>
2023-06-20 23:11:27 +0300

    feat: добавил счетчик соединений

--- QOTD_example/QOTD_server_example.py
+++ QOTD_example/QOTD_server_example.py
@@ -6,8 +6,16 @@ from twisted.internet.protocol import Protocol, Factory
 class QOTD(Protocol):
     '''QOTD (Quote of the Day)'''
 
+    def __init__(self, factory):
+        self.factory = factory
+
     def connectionMade(self):
-        logger.info(f'Подключился клиент')
+        '''
+        В событии connectionMade обычно происходит установка объекта соединения,
+        а также любые начальные приветствия
+        '''
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        logger.info(f"Сейчас {self.factory.numProtocols} открытых соединений.\n")
         self.transport.write("Вы кто такие? Я вас не звал! Идите нахуй!\r\n".encode())
         self.transport.loseConnection()
 
--- echo_example/echo_server_example.py
+++ echo_example/echo_server_example.py
@@ -4,9 +4,19 @@ from twisted.internet.protocol import Protocol, Factory
 
 
 class Echo(Protocol):
+    def __init__(self, factory):
+        self.factory = factory
+
     def dataReceived(self, data):
         logger.info(f'Получено от клиента:\n> {data.decode()}')
         self.transport.write(data)
+
+    def connectionMade(self):
+        self.factory.numProtocols = self.factory.numProtocols + 1
+        logger.info(f"Сейчас {self.factory.numProtocols} открытых соединений.\n")
+
+    def connectionLost(self, reason):
+        self.factory.numProtocols = self.factory.numProtocols - 1
 
 
 class EchoFactory(Factory):
